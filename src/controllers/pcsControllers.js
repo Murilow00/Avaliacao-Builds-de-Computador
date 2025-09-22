095aad4d3a0a2b432ba7349c67cca00a14570576
Murilow00 <milanmurilo7@gmail.com>
2025-09-22 14:56:43 -0300

    finaliza create

--- src/controllers/pcsControllers.js
+++ src/controllers/pcsControllers.js
@@ -5,7 +5,7 @@ const { builds } = dados;
 
 const getAllBuilds = (req, res) => {
     let resultado = builds;
-    const {categoria} = req.query
+    const {categoria,preco} = req.query
 
     if(categoria){
         resultado = resultado.filter(a => a.categoria.toLowerCase().includes(categoria.toLowerCase()))
@@ -37,5 +37,36 @@ const getBuildById = (req, res) => {
     });
 };
 
+const criarBuild = (req, res) => {
+    const {nomeBuild, processador, placaVideo, ram, armazenamento, preco, categoria, desempenho } = req.body;
 
-export { getAllBuilds, getBuildById,  }
+    if(!processador || !ram || !armazenamento || !preco || !desempenho){
+        return res.status(404).json({
+            sucess: false,
+            message: "Todos os computadores precisam de um processador, memoria Ram, armazenamento, preço(nada é de graça) e de seu desempenho"
+        });
+    }
+
+    const novoPc = {
+        id:builds.length + 1,
+        nomeBuild: nomeBuild,
+        processador: processador,
+        placaVideo: placaVideo,
+        ram: ram,
+        armazenamento: armazenamento,
+        preco: preco,
+        categoria: categoria,
+        desempenho: desempenho
+    }
+
+    builds.push(novoPc)
+
+    return res.status(200).json({
+        sucess: true,
+        message: "Nova Build de Compudador adicionada com sucesso",
+        build: novoPc
+    })
+}
+
+
+export { getAllBuilds, getBuildById, criarBuild }
--- src/routes/pcsRoutes.js
+++ src/routes/pcsRoutes.js
@@ -1,10 +1,10 @@
 import express from "express"
-import { getAllBuilds, getBuildById,  } from "../controllers/pcsControllers.js"
+import { getAllBuilds, getBuildById, criarBuild  } from "../controllers/pcsControllers.js"
 
 const router = express.Router()
 
 router.get("/", getAllBuilds);
 router.get("/:id", getBuildById);
-
+router.post("/", criarBuild)
 
 export default router;
